9c3433e2b65b2b4ccac5086f72fd8079d923b7bc
GermanSamosa <mat_rao@hotmail.com>
2022-09-18 09:45:47 -0400

    added and tried bootstrap css to app as a test

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,15 +1,16 @@
-import Button from 'react-bootstrap/Button';
-import './App.css';
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-  <Button as="a" variant="primary">
-    Button as link
-  </Button>
-  <Button as="a" variant="success">
-    Button as link
-  </Button>
+        <Button as="a" variant="primary">
+          Button as link
+        </Button>
+        <Button as="a" variant="success">
+          Button as link
+        </Button>
     </div>
   );
 }
