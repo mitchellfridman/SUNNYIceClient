d97a88188c4cf96d4bc2bc3b4999b4a41a98a6e0
KristyHoffman <kristyhoffman98@gmail.com>
2022-09-21 13:05:03 -0400

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,11 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
 
-/*function App() {
+/*import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Flavours } from "./pages/Flavours";
+import { Build } from "./pages/Build";
+import { Order } from "./pages/Order";
+
+function App() {
   return (
     <div className="App">
     {" "}
     <Router>
+    <div> NAVBAR </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -16,6 +23,7 @@ import Home from "./pages";
         <Route path="/flavours" element={<Flavours />} /> *Should active and inactive flavours be the same page with two sections or two separate pages?
         <Route path="/build" element={<Build />} /> *This is the page you will build your cone
         <Route path="/order" element={<Register />} /> *Page to confirm and place order, generates QR code
+        <Route path="*" element{<h1> PAGE NOT FOUND </h1>} />
       </Routes>
       </Router>
       </div>
