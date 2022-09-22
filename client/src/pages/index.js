7358a4323ea71f0caf5890b71a04df660e967781
GermanSamosa <mat_rao@hotmail.com>
2022-09-22 09:13:20 -0400

    small random changes

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,20 +1,22 @@
 import React from "react";
-import FlavorsHeroSection from "../components/Flavors/flavorsHeroSection";
-import Header from "../components/Header/header";
-import HeroSection from "../components/HeroSection/herosection";
+// import FlavorsHeroSection from "../components/Flavors/flavorsHeroSection";
+// import Header from "../components/Header/header";
+// import HeroSection from "../components/HeroSection/herosection";
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Login from './login'
 // import Register from "./register";
 // import Flavours from "./flavours";
 // import Build from "./build";
+import Build from "./build";
 
 
 function Home() {
   return (
     <>
-      <Header />
-      <HeroSection />
-      <FlavorsHeroSection/>
+    <Build/>
+      {/* <Header />
+      <HeroSection /> */}
+      {/* <FlavorsHeroSection/> */}
       {/* <Router>
       <Routes>
         <Route path="/" element={<Home />} />
