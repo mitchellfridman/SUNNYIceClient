e4c2b6a06b3662edafac2cce4fe24c4548cd6429
GermanSamosa <mat_rao@hotmail.com>
2022-09-26 21:46:42 -0400

    cleaning things up

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,13 +2,6 @@ import React from "react";
 // import FlavorsHeroSection from "../components/Flavors/flavorsHeroSection";
 import Header from "../components/Header/header";
 import HeroSection from "../components/HeroSection/herosection";
-// import Build from "./build";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Login from './login'
-// import Register from "./register";
-// import Flavours from "./flavours";
-// import Build from "./build";
-
 
 function Home() {
   return (
